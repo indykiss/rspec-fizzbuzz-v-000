199e1219257ee8b3cdf297fc7a27b815ec63d1fd
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-07-21 03:16:49 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,12 +4,12 @@
 
 def fizzbuzz (num)
 
-    if num % 15 == 0 
-      puts "Fizzbuzz"
+    if num % 3 == 0 
+      puts "Fizz"
     elsif num % 5 == 0 
       puts "Buzz"
-    elsif num % 3 == 0 
-      puts "Fizz"
+    elsif num % 15 == 0 
+      puts "FizzBuzz"
     else 
       puts "nil"
    end
