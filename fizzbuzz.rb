070bf4ba80b393efeee2481174c990ad2371db4c
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-07-21 03:20:55 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,12 +4,12 @@
 
 def fizzbuzz (num)
 
-    if num % 3 == 0 
-      print "Fizz"
+    if num % 3 == 0 && num % 5 == 0
+      print "FizzBuz"
+    elsif num % 3 == 0 
+      print "FizzBuzz"
     elsif num % 5 == 0 
       print "Buzz"
-    elsif num % 5 == 0 && num % 3 == 0
-      print "FizzBuzz"
     else 
       print "nil"
    end
