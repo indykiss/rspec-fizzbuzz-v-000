6e2ded65605cb37173cb3dc88a6455c5e0780b54
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-07-21 03:10:22 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,19 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+
+def fizzbuzz (num)
+
+    if num % 15 == 0 
+      puts "Fizzbuzz"
+    elsif num % 5 == 0 
+      puts "Buzz"
+    elsif num % 3 == 0 
+      puts "Fizz"
+    else 
+      puts "nil"
+   end
+end
+
+
+
